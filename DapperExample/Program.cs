59a300c458d32fc85b6a7087f3636069e38a7672
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-03-21 18:30:52 +0200

    First commit

---
+++ DapperExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dapper!");
+            UsingUnitOfWork();
+
+            Console.Read();
+
+        }
+
+        private static void UsingUnitOfWork()
+        {
+            
+        }
+    }
+}
