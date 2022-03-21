484ae501cfc711e3c82d66077a035c944e949a3d
MihailCoscodan <mihail.coscodan@acconomy.com>
2022-03-21 19:13:12 +0200

    Select example

---
+++ DapperExample/Model/Firma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExample.Model
+{
+    public class Firma
+    {
+        public int FIRMA_ID { get; set; }
+        public string NUME { get; set; }
+        public string COD_FISCAL { get; set; }
+        public string ADRESA { get; set; }
+
+    }
+}
--- DapperExample/Program.cs
+++ DapperExample/Program.cs
@@ -1,21 +1,43 @@
-﻿using System;
+﻿using Dapper;
+using DapperExample.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DapperExample
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string _connectionString = @"Server=93.115.141.254,10433;Database=MAGAZIN;User Id=itstep;Password=itstep++;";
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello Dapper!");
-            UsingUnitOfWork();
-
+            await UsingCRUD();
             Console.Read();
-
         }
 
-        private static void UsingUnitOfWork()
+        private static async Task UsingCRUD()
         {
-            
+            IDbConnection db = new SqlConnection(_connectionString);
+
+            // select all firms
+            var firms = await db.QueryAsync<Firma>(@"SELECT TOP (1000) [FIRMA_ID]
+                                                      ,[NUME]
+                                                      ,[COD_FISCAL]
+                                                      ,[ADRESA]
+                                                  FROM[MAGAZIN].[dbo].[FIRMA]");
+
+            foreach (var firma in firms)
+            {
+                Console.WriteLine($" {firma.FIRMA_ID} {firma.NUME} {firma.COD_FISCAL} {firma.ADRESA}");
+            }
+
+            // Insert example
+
+             
         }
     }
 }
